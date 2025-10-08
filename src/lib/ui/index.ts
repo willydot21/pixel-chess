a31619e5042291ac6448106240ef612c8ee0a308
willydot21 <willyusername.getall@gmail.com>
2025-10-08 21:23:38 +0200

    got scale factor.

---
+++ src/lib/ui/index.ts
@@ -0,0 +1,19 @@
+
+
+export const { $, $$ } = {
+  $: (selector: string) => document.querySelector(selector),
+  $$: (selector: string) => document.querySelectorAll(selector)
+}
+
+
+const statusText = $('#status') as HTMLSpanElement;
+export const updateStatusText = (text: string) => {
+  statusText.textContent = text;
+  return statusText;
+}
+
+const turnText = $('#turn') as HTMLSpanElement;
+export const updateTurnText = (text: string) => {
+  turnText.textContent = text;
+  return turnText;
+}
